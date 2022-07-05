ce51a4f38cd885017321708646f4171b69e7d672
Kristoff Villanueva <kristoffer.villanueva18@gmail.com>
2022-07-05 23:32:45 +0800

    start movie app

---
+++ movieapp/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	const name = "John";
+	const isNameShowing = false;
+	return (
+		<div className="App">
+			<h1>Hello, {isNameShowing ? name : "someone"}</h1>
+		</div>
+	);
+}
+
+export default App;
